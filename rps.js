988c08a8f02dca6f5c4ec2b8966f969dea5b6a89
tjnotinjapan <tjnotinjapan@gmail.com>
2026-07-23 09:31:18 +0900

    Updating RPS game to add UI. Added buttons and logic for them

--- rps.js
+++ rps.js
@@ -6,16 +6,12 @@ function getComputerChoice() {
     // Return "rock" for 0, "paper" for 1, and "scissors" for 2
     if (getRandomNumber == 0) {
         return 'rock';
+        console.log('rock')
     } else if (getRandomNumber == 1) {
         return 'paper';
     } else {
         return 'scissors';
     }
-}
-
-function getHumanChoice() {
-    // User prompt to get user choice
-    return prompt("Rock, Paper, or Scissors? ");
 }
 
 // Global declare of human and computer score (default 0)
@@ -24,74 +20,75 @@ let computerScore = 0;
 
 // Send alert message of round result
 function roundResult(pick1, pick2) {
-    alert(pick1 + ' beats ' + pick2);
-}
-
-
-// Play 5 rounds of RPS
-function playGame() {
-
-    // User will play five round of RPS
-    // Each round, the human or computer score increases depending on results
-
-    for (i = 0; i < 5; i ++) {
-
-        function playRound(humanPick, computerPick) {
-            // Compare humanPick and computerPick to determine winner
-            // Rock beats scissors, scissors beats paper, paper beats rock
-            // Same pick is a tie, no winner
-            if (humanPick == computerPick) {
-                alert('Tie!');
-                return;
-            } else if (humanPick == 'rock') {
-                if (computerPick == 'scissors') {
-                    humanScore += 1;
-                    roundResult(humanPick, computerPick);
-                } else {
-                    computerScore += 1;
-                    roundResult(computerPick, humanPick);
-                }
-            } else if (humanPick == 'paper') {
-                if (computerPick == 'rock') {
-                    humanScore += 1;
-                    roundResult(humanPick, computerPick);
-                } else {
-                    computerScore += 1;
-                    roundResult(computerPick, humanPick);
-                }
-            } else {
-                if (computerPick == 'paper') {
-                    humanScore += 1;
-                    roundResult(humanPick, computerPick);
-                } else {
-                    computerScore += 1;
-                    roundResult(computerPick, humanPick);
-                }
-            }
-        }
-
-        // Get choices from human and computer
-        const humanChoice = getHumanChoice();
-        const computerChoice = getComputerChoice();
-            
-        // Play a round
-        playRound(humanChoice, computerChoice);
-    }
-
-    // Display winner at end of game using alert
-        // Compare human and computer scores
-    if (humanScore > computerScore) {
-        alert('Human wins! ' + humanScore + ' to ' + computerScore);
-        console.log("Human wins!");
-    } else if (humanScore < computerScore) {
-        alert('Computer wins! ' + computerScore + ' to ' + humanScore);
-        console.log("Computer wins!");
+    // If tie (same picks)
+    if (pick1 === pick2) {
+        // 
+        alert('tie');
     } else {
-        alert("It's a tie!");
-        console.log("Nobody wins.");
+        // 
+        alert(pick1 + ' beats ' + pick2);
     }
 }
 
-playGame();
 
+function playRound(humanPick, computerPick) {
+    console.log('polayed' + humanPick + computerPick)
+    // Compare humanPick and computerPick to determine winner
+    // Rock beats scissors, scissors beats paper, paper beats rock
+    // Same pick is a tie, no winner
+    if (humanPick == computerPick) {
+        roundResult(humanPick, computerPick);
+        return;
+    } else if (humanPick == 'rock') {
+        if (computerPick == 'scissors') {
+            humanScore += 1;
+            roundResult(humanPick, computerPick);
+        } else {
+            computerScore += 1;
+            roundResult(computerPick, humanPick);
+        }
+    } else if (humanPick == 'paper') {
+        if (computerPick == 'rock') {
+            humanScore += 1;
+            roundResult(humanPick, computerPick);
+        } else {
+            computerScore += 1;
+            roundResult(computerPick, humanPick);
+        }
+    } else {
+        if (computerPick == 'paper') {
+            humanScore += 1;
+            roundResult(humanPick, computerPick);
+        } else {
+            computerScore += 1;
+            roundResult(computerPick, humanPick);
+        }
+    }
+
+    // Update scores
+    const humanScoreCount = document.querySelector('#human-score');
+    const computerScoreCount = document.querySelector('#computer-score');
+
+    humanScoreCount.innerHTML = humanScore;
+    computerScoreCount.innerHTML = computerScore;
+}
+
+// RPS button choices
+let buttonChoices = document.querySelector('#button-choices');
+
+// Listen for which choice human player makes and send to game
+buttonChoices.addEventListener('click', (event) => {
+    let target = event.target;
+        switch(target.id) {
+            case 'rock':
+                playRound('rock', getComputerChoice());
+                break;
+            case 'paper':
+                playRound('paper', getComputerChoice());
+                break;
+            case 'scissors':
+                playRound('scissors', getComputerChoice());
+                break;
+        }
+});
 
